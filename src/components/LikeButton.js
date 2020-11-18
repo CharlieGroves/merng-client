26d77812a5efce07b74282b6537fd15f31d4966b
Charlie Groves <ctrg04@gmail.com>
2020-11-18 15:55:52 +0000

    delete button glitch on mobile fixed

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -9,7 +9,6 @@ function LikeButton({ user, post: { id, likeCount, likes }}) {
     const [liked, setLiked] = useState(false)
 
     useEffect(() => {
-        liked = true;
         if(user && likes.find(like => like.username === user.username)) {
             setLiked(true)
         } else setLiked(false)
