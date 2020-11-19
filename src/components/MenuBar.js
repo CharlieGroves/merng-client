aea0c77520627d28978bd041d32944874965745b
Charlie Groves <ctrg04@gmail.com>
2020-11-19 19:37:22 +0000

    actually did confirm logout

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { Menu, Segment, Confirm } from 'semantic-ui-react';
+import { Menu, Confirm } from 'semantic-ui-react';
 import { Link } from 'react-router-dom'
 
 import { AuthContext } from '../context/auth'
